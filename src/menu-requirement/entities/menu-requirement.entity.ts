9cab271b7fcb23c7e6565f98a8efb63f53db2bee
Rafael Belmont <rafaelmagalhaes12323@gmail.com>
2025-05-10 17:03:54 -0300

    feat: menu requirement entity

--- src/menu-requirement/entities/menu-requirement.entity.ts
+++ src/menu-requirement/entities/menu-requirement.entity.ts
@@ -1,1 +1,70 @@
-export class MenuRequirement {}
+import {
+  AutoIncrement,
+  Column,
+  CreatedAt,
+  DataType,
+  DeletedAt,
+  Model,
+  NotNull,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'tb_menu-requirements',
+  modelName: 'MenuRequirement',
+  timestamps: true,
+})
+export class MenuRequirement extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  declare id: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare calories: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare carbohydrates: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare proteins: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare fats: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare fiber: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare sugar: number;
+
+  @NotNull
+  @Column({ type: DataType.DECIMAL, allowNull: false })
+  declare sodium: number;
+
+  @CreatedAt
+  @Column({
+    field: 'created_at',
+  })
+  declare createdAt: Date;
+
+  @DeletedAt
+  @Column({
+    field: 'deleted_at',
+  })
+  declare deletedAt: Date;
+
+  @UpdatedAt
+  @Column({
+    field: 'updated_at',
+  })
+  declare updateAt: Date;
+}
--- src/menu-requirement/menu-requirement.module.ts
+++ src/menu-requirement/menu-requirement.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MenuRequirementService } from './menu-requirement.service';
 import { MenuRequirementController } from './menu-requirement.controller';
 import { MenuRequirementRepository } from './menu-requirement.repository';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [MenuRequirementController],
   providers: [MenuRequirementService, MenuRequirementRepository],
 })
