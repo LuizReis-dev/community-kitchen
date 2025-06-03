6d89eb25fbf2d0db5a01438676de7c2144c8f9f5
Luiz Reis <reisluiz05@gmail.com>
2025-06-02 21:55:22 -0300

    feature: user authentication with remote api

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,29 @@ import { MenuRequirementModule } from './menu-requirement/menu-requirement.modul
 import { DailyEventModule } from './daily-event/daily-event.module'
 import { CustomerModule } from './customer/customer.module'
 import { MenuAttendanceModule } from './menu-attendance/menu-attendance.module';
+import { APP_GUARD } from '@nestjs/core'
+import { AuthModule } from './auth/auth.module'
+import { AuthGuard } from './auth/auth.guard'
 
 @Module({
-	imports: [
-		FoodModule,
-		DatabaseModule,
-		DishModule,
-		MenuModule,
-		MenuRequirementModule,
-		DailyEventModule,
-		CustomerModule,
-		MenuAttendanceModule
-	],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [
+    FoodModule,
+    DatabaseModule,
+    DishModule,
+    MenuModule,
+    MenuRequirementModule,
+    DailyEventModule,
+    CustomerModule,
+    MenuAttendanceModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/auth/services/remote-token-validator.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import axios from 'axios';
+import { TokenValidator } from '../interfaces/token-validator.interface';
+import { User } from '../interfaces/user.interface';
+
+@Injectable()
+export class RemoteTokenValidator implements TokenValidator {
+    async validateToken(token: string): Promise<User> {
+        try {
+            const response = await axios.post<{ user: User }>(
+                process.env.REMOTE_TOKEN_VALIDATOR_URL ?? "",
+                {},
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                },
+            );
+            return response.data.user;
+        } catch (error) {
+            throw new UnauthorizedException('Token inválido ou expirado');
+        }
+    }
+}
