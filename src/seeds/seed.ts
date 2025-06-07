c3d1f4f82097ec526fe2b2a4a29da152311fdf59
Rafael Belmont <rafaelmagalhaes12323@gmail.com>
2025-06-06 23:27:16 -0300

    feat: creating seed configuration files and sync

---
+++ src/seeds/seed.ts
@@ -0,0 +1,23 @@
+// src/seed.ts
+import { sequelize } from './config/sequelize-config.seed';
+import { createFoods } from './food.seed';
+import { createDishes } from './dish.seed'; 
+
+async function runSeeds() {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log('Banco de dados sincronizado com sucesso!');
+
+    await createFoods(sequelize);
+    await createDishes(sequelize);
+
+    console.log('Todas as seeds foram executadas com sucesso!');
+  } catch (error) {
+    console.error('Erro ao executar as seeds:', error);
+    process.exit(1);
+  } finally {
+    await sequelize.close();
+  }
+}
+
+runSeeds();
